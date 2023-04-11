b5bc251675fb0eba25906edbbe8ff3cc2247caf1
Manishshrivastva60 <manish.shrivastva60@gmail.com>
2023-04-11 21:30:35 +0530

    commit

---
+++ forth.py
@@ -0,0 +1,39 @@
+print("hello friends")
+print('i am manish shrivastva')
+print("""i am from bihar at gopalganj""")
+print('''there is my six member of my family''')
+
+print("how are you,rahul")
+
+
+print("this is a 'apple'")
+print('this is a "apple"')
+print("this is a \"apple")
+print("this is a \"apple\"")
+
+print("i an swami \chandra")
+print("i an swami \\nathan")
+print("i an swami \\rahul")
+
+print("i an manish""shrivastva")
+print("i an manish","shrivastva")
+print("i an manish","shrivastva",sep=" ")
+print("i an manish","shrivastva",sep="kumar")
+
+print("apple","banana","pineapple")
+print("apple","banana","pineapple",sep="$\nmanish\t")
+print("apple","banana","pineapple",sep=",")
+print("hello friends",end=" ");print("hello friends", end=" ")
+print("thia is vivek")
+
+print("apple","banana","pineapple",sep=",",end=",india,")
+print("apple","banana","pineapple",sep=",")
+print("this is manish \n\kumar","india",end="\t")
+
+print("this is my first python program",sep="\n")
+print("this is my first python program",sep=",")
+
+a=10
+b=20
+c=a+b
+print(c)
